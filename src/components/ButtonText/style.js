25d78e85de304544388703092d3e090d8a8aaa79
WagnerRodrigues <felten19@gmail.com>
2023-04-24 20:35:56 -0300

    Criado component Button

---
+++ src/components/ButtonText/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+  background-color: red;
+  border-radius: 10px;
+  background-color: #ff859b;
+  color: black;
+
+  width: 207px;
+  height: 48px;
+`;
