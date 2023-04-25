a622a54fd2f4a7f329a1c6c038b4957655bb55cc
WagnerRodrigues <felten19@gmail.com>
2023-04-25 15:37:00 -0300

    Criado tag com genero do filme

---
+++ src/components/Tag/index.jsx
@@ -0,0 +1,9 @@
+import {Container} from "./style";
+
+export default function Tag({title}){
+    return(
+        <Container>
+            {title}
+        </Container>
+    );
+}
---
+++ src/components/Tag/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.span `
+    border: 1px solid #312E38;
+    border-radius: 10px;
+    background-color: #413B3A;
+    color: white;
+    padding: 12px;
+
+`;
